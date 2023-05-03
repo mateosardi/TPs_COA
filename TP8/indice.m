6915acdb6212322cce45b26e2bc3683312c18e58
mateosardi <102429611+mateosardi@users.noreply.github.com>
2023-05-03 11:48:32 -0300

    Create indice.m

---
+++ TP8/indice.m
@@ -0,0 +1,8 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Rutina: Indice
+% x: Estado actual
+% k: Paso en el que se encuentra
+% accion: Accion de control aplicada
+function [y]=indice(k,x,accion)
+y=(2+accion)*exp(-x);
+%Fin rutina indice
