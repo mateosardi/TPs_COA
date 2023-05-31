0f4a71f371a68a66b71c81821f10187b37820886
mateosardi <102429611+mateosardi@users.noreply.github.com>
2023-05-31 11:47:39 -0300

    Update mopdm2_motor.m

--- TP9/Motor/mopdm2_motor.m
+++ TP9/Motor/mopdm2_motor.m
@@ -11,20 +11,18 @@
 function [X]=mopdm2_motor(tiempo_etapa,xant,accion)
 Laa=366e-6; J=5e-9;Ra=55.6;B=0;Ki=6.49e-3;Km=6.53e-3;
 Tl = 0.002;
-h=1e-2;tiempo=(100/h); t=0:h:tiempo*h;
-theta=0:h:tiempo*h; w=0:h:tiempo*h; ia=0:h:tiempo*h;
-ip=0:h:tiempo*h; u=linspace(0,0,tiempo+1);
+h=1e-7;
 
 ia = xant(1);
 w = xant(2);
 theta= xant(3);
 
 Va = accion;
-
+ip=0;
 for i=1:tiempo_etapa/h
     ip = -Ra/Laa*ia - Km/Laa*theta+Va/Laa;
     w = Ki/J * ia - B/J*theta - Tl/J;
     ia = ia + h*ip;
     theta = theta + h*w;   
 end
-X=[theta;w; ia];
+X=[ia;w;theta];
