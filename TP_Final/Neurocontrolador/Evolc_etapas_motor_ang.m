5012b532d2bec4add227a863440be20b3081caec
mateosardi <102429611+mateosardi@users.noreply.github.com>
2023-06-29 19:48:14 -0300

    Update and rename Evolc_etapas_motor.m to Evolc_etapas_motor_ang.m

--- TP_Final/Neurocontrolador/Evolc_etapas_motor_ang.m
+++ TP_Final/Neurocontrolador/Evolc_etapas_motor_ang.m
@@ -1,7 +1,7 @@
 % clc; clear all; 
 close all;
 TamanioFuente=14;
-CI=10;
+CI=2;
 titaRef=0;
 color='.r';colorc='r';
 tiempo_etapa =t_etapa;
@@ -23,7 +23,7 @@ sal=zeros(4,1);   y4=zeros(1,etapas);y1=zeros(1,etapas);
 u_a=0; Mw=zeros(4,etapas-1); costo=[];costo(1)=0;consigna=[];consigna(1)=0;
 color='.-k';
 
-sigma_ia=.01;sigma_theta=.1;
+sigma_ia=.01;sigma_theta=.1;sigma_omega=.1;
 
 % ia = xant(1);
 % i_f = xant(2);
@@ -31,9 +31,10 @@ sigma_ia=.01;sigma_theta=.1;
 % theta= xant(4);
 u=0;
 e=zeros(etapas+3,1);
+
 for k=1:etapas-1
     VX(:,k)=sx;
-    entrada = [sx(1); sx(2);sx(3);sx(4)]+0.5*diag([sigma_ia 0 0 sigma_theta])*randn(4,1);
+    entrada = [sx(1); sx(2);sx(3);sx(4)]+0*diag([sigma_ia 0 sigma_omega sigma_theta])*randn(4,1);
     %Medicion ruidosa
     y1(k)=entrada(1); % corriente armadura
     y2(k)=entrada(2); % corriente de campo
@@ -44,19 +45,17 @@ for k=1:etapas-1
     y11=[pmntanh(s1); 1];
     s2 = W2a * y11;
     
-    kk=k+3;
-    e(kk)=titaRef-entrada(4); %ERROR
-    u = u + A1*e(kk) + B1*e(kk-1) + C1*e(kk-2); % Acción de control utilizando PID
-%     s2=u; % Implementar PID
-
     consigna(k)=s2;
-%     sx(2)=2.5;
+
     sal=mopdm2_motor(tiempo_etapa,sx,torque,consigna(k),Vf);
+    wp(k) = sal(5);
+    sal = sal(1:4);
     sx=sal(:,1);
     costo_p(k)=indice_g_motor(sal,consigna(k));
     costo(k+1)=costo(k)+indice_g_motor(sx,consigna(k));
     u_a=consigna(k);    
 end
+
 
 NPD=costo(etapas);
 
@@ -64,11 +63,8 @@ y1(k+1)=sal(1,1); % corriente armadura
 y2(k+1)=sal(2,1); % corriente de campo
 y3(k+1)=sal(3,1); % velocidad angular
 y4(k+1)=sal(4,1); % angulo motor
+wp(k+1) = wp(k);
 
-
-
-% % subplot(4,1,3),plot(To*xx1,costo,color),ylabel('Costo'),grid on,hold on; %,axis([0 etapas 0 6.1]);
-% subplot(4,1,4),plot(To*xx1(1:length(uo)),uo,color),ylabel('Accion de control'),xlabel('Tiempo [seg.]'),grid on,hold on;
 
 uo=consigna; To=t_etapa;
 figure;
@@ -81,8 +77,6 @@ ylabel('Accion de control'),xlabel('Tiempo [seg.]'),title('Accion de control');
 subplot(2,2,2);
 plot(To*xx1,y1);title('Corriente de armadura ia'); hold on;
 xlabel('tiempo[s]');ylabel('Corriente[A]');
-% plot(To*xx1,y2);title('Corriente de campo'); hold on;
-% xlabel('tiempo[s]');ylabel('Corriente[A]');
 subplot(2,2,3);
 plot(To*xx1,y3);title('Velocidad \omega'); hold on;
 xlabel('tiempo[s]');ylabel('Velocidad[rad/s]'); 
@@ -90,7 +84,6 @@ subplot(2,2,4);
 plot(To*xx1,y4);title('Angulo \theta'); hold on;
 xlabel('tiempo[s]');ylabel('Angulo[rad]');
 
-fprintf('Media: %.4f\n',mean(y4))
-fprintf('Varianza: %.4f\n',var(y4))
-
-
+t_theta = [To*xx1;y4]';
+t_omega = [To*xx1;y3]';
+t_wp = [To*xx1;wp]';
